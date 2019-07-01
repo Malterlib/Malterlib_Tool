66d58f622575f951a14ced90323f1e028b8b099d
Erik Olofsson <erik@olofsson.info>
2019-07-01 11:42:52 +0200

    [Malterlib/Tool] Refactor to move only observation of futures

--- Apps/MTool/Source/Malterlib_Tool_App_MTool_JSON.cpp
+++ Apps/MTool/Source/Malterlib_Tool_App_MTool_JSON.cpp
@@ -44,7 +44,7 @@ public:
 		else
 			Contents.f_SetResult(CFile::fs_ReadStringFromFile(CFile::fs_GetExpandedPath(SourcePath, true)));
 
-		CEJSON JSON = CEJSON::fs_FromString(Contents.f_CallSync(60.0));
+		CEJSON JSON = CEJSON::fs_FromString(Contents.f_MoveFuture().f_CallSync(60.0));
 
 		if (Params.f_IsEmpty())
 		{
