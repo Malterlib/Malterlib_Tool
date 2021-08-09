4c7f8c9a1285a63a8c0425ab0629cb742628e59a
Erik Olofsson <erik@olofsson.info>
2021-08-09 18:30:49 +0200

    [Malterlib/Tool] Update import cache

--- ImportCache/External_CMake-Windows-x64-Debug/Source/cmVersionConfig.h
+++ ImportCache/External_CMake-Windows-x64-Debug/Source/cmVersionConfig.h
@@ -3,6 +3,6 @@
 #define CMake_VERSION_MAJOR 3
 #define CMake_VERSION_MINOR 20
 #define CMake_VERSION_PATCH 4
-#define CMake_VERSION_SUFFIX "g78fa856"
+#define CMake_VERSION_SUFFIX "gaf0f4ac"
 #define CMake_VERSION_IS_DIRTY 0
-#define CMake_VERSION "3.20.4-g78fa856"
+#define CMake_VERSION "3.20.4-gaf0f4ac"
