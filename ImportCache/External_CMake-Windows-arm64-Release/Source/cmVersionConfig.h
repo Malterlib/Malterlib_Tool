ae70752239eca879161afa5344ec9cb9e429425a
Erik Olofsson <erik@olofsson.info>
2023-05-24 18:30:31 +0200

    [Malterlib/Tool] Add Windows arm64 support

---
+++ ImportCache/External_CMake-Windows-arm64-Release/Source/cmVersionConfig.h
@@ -0,0 +1,10 @@
+/* Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+   file Copyright.txt or https://cmake.org/licensing for details.  */
+#pragma once
+
+#define CMake_VERSION_MAJOR 3
+#define CMake_VERSION_MINOR 26
+#define CMake_VERSION_PATCH 3
+#define CMake_VERSION_SUFFIX "g01bd1c3"
+#define CMake_VERSION_IS_DIRTY 0
+#define CMake_VERSION "3.26.3-g01bd1c3"
