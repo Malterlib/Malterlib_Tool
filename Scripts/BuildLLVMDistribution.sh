e9117b9f0744be7b700ae249f53136a2ae37c964
Erik Olofsson <erik@olofsson.info>
2024-05-08 11:06:07 +0200

    [Malterlib/Tool] Use default release cmake cache when building llvm
    Add support for building on Windows

--- Scripts/BuildLLVMDistribution.sh
+++ Scripts/BuildLLVMDistribution.sh
@@ -27,6 +27,38 @@ if [[ "$BuildIncremental" != "true" ]]; then
 	rm -rf "$DistributionDir/"*
 fi
 
+ExtraCMake="-G Ninja"
+
+ExtraCMake="$ExtraCMake -DLLVM_USE_STATIC_ZSTD=ON"
+ExtraCMake="$ExtraCMake -DBOOTSTRAP_LLVM_USE_STATIC_ZSTD=ON"
+ExtraCMake="$ExtraCMake -DBOOTSTRAP_BOOTSTRAP_LLVM_USE_STATIC_ZSTD=ON"
+
+ExtraCMake="$ExtraCMake -DLLVM_INSTALL_TOOLCHAIN_ONLY=ON"
+ExtraCMake="$ExtraCMake -DBOOTSTRAP_LLVM_INSTALL_TOOLCHAIN_ONLY=ON"
+ExtraCMake="$ExtraCMake -DBOOTSTRAP_BOOTSTRAP_LLVM_INSTALL_TOOLCHAIN_ONLY=ON"
+
+ExtraCMake="$ExtraCMake -DLLVM_RELEASE_ENABLE_RUNTIMES=compiler-rt;libcxx;libcxxabi;libunwind"
+ExtraCMake="$ExtraCMake -DLLVM_RELEASE_ENABLE_PROJECTS=clang;clang-tools-extra;lld"
+
+if [[ "$MalterlibPlatform" == "Linux" ]] && [[ "$MalterlibArch" == "x86" ]]; then
+	ExtraCMake="$ExtraCMake -DLLVM_ENABLE_LTO=OFF"
+	ExtraCMake="$ExtraCMake -DBOOTSTRAP_LLVM_ENABLE_LTO=OFF"
+
+	ExtraCMake="$ExtraCMake -DSANITIZER_ALLOW_CXXABI=OFF"
+	ExtraCMake="$ExtraCMake -DBOOTSTRAP_SANITIZER_ALLOW_CXXABI=OFF"
+
+	ExtraCMake="$ExtraCMake -DLLVM_HOST_TRIPLE=i686-unknown-linux-gnu"
+	ExtraCMake="$ExtraCMake -DBOOTSTRAP_LLVM_HOST_TRIPLE=i686-unknown-linux-gnu"
+
+	ExtraCMake="$ExtraCMake -DCMAKE_EXE_LINKER_FLAGS_INIT=-latomic"
+	ExtraCMake="$ExtraCMake -DCMAKE_SHARED_LINKER_FLAGS_INIT=-latomic"
+	ExtraCMake="$ExtraCMake -DBOOTSTRAP_CMAKE_EXE_LINKER_FLAGS_INIT=-latomic"
+	ExtraCMake="$ExtraCMake -DBOOTSTRAP_CMAKE_SHARED_LINKER_FLAGS_INIT=-latomic"
+
+	ExtraCMake="$ExtraCMake -DLLVM_TARGETS_TO_BUILD=X86"
+	ExtraCMake="$ExtraCMake -DBOOTSTRAP_LLVM_TARGETS_TO_BUILD=X86"
+fi
+
 BuildCompilerLTO()
 {
 	mkdir -p "$RootDir/build/dist_temp"
@@ -35,22 +67,35 @@ BuildCompilerLTO()
 	popd
 
 	if [[ "$BuildIncremental" != "true" ]]; then
+		ExtraCMake="$ExtraCMake -DLLVM_RELEASE_ENABLE_PGO=ON"
+		ExtraCMake="$ExtraCMake -DBOOTSTRAP_LLVM_VP_COUNTERS_PER_SITE=3"
+		ExtraCMake="$ExtraCMake -DBOOTSTRAP_BOOTSTRAP_CMAKE_INSTALL_PREFIX=$DistributionDir"
+
+		if [[ "$MalterlibPlatform" != "Linux" ]]; then
+			ExtraCMake="$ExtraCMake -DCLANG_PGO_TRAINING_PROFILES_DIR=$BuildDir/profiles/"
+			ExtraCMake="$ExtraCMake -DBOOTSTRAP_CLANG_PGO_TRAINING_PROFILES_DIR=$BuildDir/profiles/"
+			ExtraCMake="$ExtraCMake -DBOOTSTRAP_BOOTSTRAP_CLANG_PGO_TRAINING_PROFILES_DIR=$BuildDir/profiles/"
+		fi
+
 		pushd "$BuildDir/dist_temp"
 			echo Building compiler
 
-			local ExtraCMake="-G Ninja"
-
-			(cmake $ExtraCMake -C "$RootDir/cmake_caches/PGO.cmake" "$MalterlibRoot/External/llvm-project/llvm")
+			(cmake $ExtraCMake -C "$MalterlibRoot/External/llvm-project/clang/cmake/caches/Release.cmake" "$MalterlibRoot/External/llvm-project/llvm")
 
 			time ninja -j${NCPUS} stage2-instrumented
 		popd
 
 		# Generate profiling data
-		pushd "$BuildDir/dist_temp/tools/clang/stage2-instrumented-bins"
-			#ninja check-all || true
-		popd
 
+		rm -rf "$BuildDir/profiles"
+		mkdir -p "$BuildDir/profiles"
+		if [[ "$MalterlibPlatform" == "macOS" ]]; then
+			export LLVM_PROFILE_FILE="$BuildDir/profiles/malterlib-%${NCPUS}m%c.profraw"
+		else
+			export LLVM_PROFILE_FILE="$BuildDir/profiles/malterlib-%${NCPUS}m.profraw"
+		fi
 		pushd "$MalterlibRoot"
+
 			#export EnableReleaseConfig=true
 			export EnableReleaseTestingConfig=true
 			export MalterlibDisableBuildSystemGeneration=true
@@ -58,11 +103,18 @@ BuildCompilerLTO()
 			export MalterlibImportUpdateCache=false
 
 			if [[ "$MalterlibPlatform" == "macOS" ]]; then
+				(source "$MalterlibRoot/BuildSystem/SharedBuildSettings.sh"; rm -rf "$MalterlibCompiledFilesSourceBase/Tests/Int"; rm -rf "$MalterlibCompiledFilesSourceBase/Tests/Out")
+
+				export EnablePlatform_macOS=true
+				export EnablePlatform_Linux=true
+
 				export EnableArchitecture_x86=false
 				export EnableArchitecture_x64=true
 				export EnableArchitecture_arm64=true
-				export EnablePlatform_macOS=true
-				export EnablePlatform_Linux=true
+
+				export EnableDebugConfig=true
+				export EnableReleaseConfig=false
+				export EnableReleaseTestingConfig=true
 
 				./mib generate --no-use-user-settings Tests --reconcile-removed=*:leave
 
@@ -86,11 +138,13 @@ BuildCompilerLTO()
 				#./mib build "Tests" "Linux" "x64" "Release (Tests)" || true # Crashes
 				#./mib build "Tests" "Linux" "x64" "Debug" || true
 				#./mib build "Tests" "Linux" "x64" "Release Testing (Tests)" || true
-			        pushd "$BuildDir/dist_temp/tools/clang/stage2-instrumented-bins"
-					ninja check-clang || true
-			        popd
+				#pushd "$BuildDir/dist_temp/tools/clang/stage2-instrumented-bins"
+				#	ninja check-clang || true
+				#popd
 
 			elif [[ "$MalterlibPlatform" == "Windows" ]]; then
+				(source "$MalterlibRoot/BuildSystem/SharedBuildSettings.sh"; rm -rf "$MalterlibCompiledFilesSourceBase/Tests/Int"; rm -rf "$MalterlibCompiledFilesSourceBase/Tests/Out")
+
 				export EnableArchitecture_x86=true
 				export EnableArchitecture_x64=true
 				export EnableArchitecture_arm64=false
@@ -107,22 +161,11 @@ BuildCompilerLTO()
 				echo "Unknow platform"
 				exit 1
 			fi
-
 		popd
-
-		# Merge profiling data
-		"$BuildDir/dist_temp/bin/llvm-profdata" merge "-output=$RootDir/build/merged.profdata" "$BuildDir/dist_temp/tools/clang/stage2-instrumented-bins/profiles/"*.profraw
 	fi
 
-	mkdir -p "$RootDir/build/dist_temp2"
-	pushd "$RootDir/build/dist_temp2"
-		ExtraCMake="$ExtraCMake -DBOOTSTRAP_CMAKE_INSTALL_PREFIX=$DistributionDir"
-		ExtraCMake="$ExtraCMake -DBOOTSTRAP_LLVM_PROFDATA_FILE=$RootDir/build/merged.profdata"
-
-		(cmake $ExtraCMake -C "$RootDir/cmake_caches/Distribution.cmake" "$MalterlibRoot/External/llvm-project/llvm")
-
-		time ninja -j${NCPUS} ClangDriverOptions
-		time ninja -j${NCPUS} stage2-install-distribution
+	pushd "$BuildDir/dist_temp"
+		time ninja -j${NCPUS} stage2-install
 	popd
 }
 
@@ -134,13 +177,11 @@ BuildCompiler()
 		local BuildDir="$PWD"
 	popd
 	pushd "$RootDir/build/dist_temp2"
-		local ExtraCMake="-G Ninja"
 		ExtraCMake="$ExtraCMake -DBOOTSTRAP_CMAKE_INSTALL_PREFIX=$DistributionDir"
 
-		(cmake $ExtraCMake -C "$RootDir/cmake_caches/Distribution.cmake" "$MalterlibRoot/External/llvm-project/llvm")
+		(cmake $ExtraCMake -C "$MalterlibRoot/External/llvm-project/clang/cmake/caches/Release.cmake" "$MalterlibRoot/External/llvm-project/llvm")
 
-		time ninja -j${NCPUS} ClangDriverOptions
-		time ninja -j${NCPUS} stage2-install-distribution
+		time ninja -j${NCPUS} stage2-install
 	popd
 }
 
---
+++ Scripts/BuildLLVMDistributionWindows.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -e
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+echo Directory: $PWD
+ScriptDir="$PWD"
+
+pushd ../../..
+	MalterlibRoot="$PWD"
+popd
+
+source "$MalterlibRoot/Malterlib/Core/Scripts/Detect.sh"
+DistributionDir="$MalterlibRoot/Binaries/MalterlibLLVM/$MalterlibPlatform/$MalterlibArch"
+
+LLVMVersion=18.1.5
+
+ExtraCMake="-G Ninja"
+RootDir="$ScriptDir"
+RootOutputDir="$ScriptDir"
+
+if [[ "$MalterlibPlatform" == "Windows" ]]; then
+	RootOutputDir="/c/CompiledFiles/llvm"
+fi
+
+NCPUS=`nproc || sysctl -n hw.ncpu`
+echo Number of CPUs: ${NCPUS}
+
+PerlPath=`find /c/Apps/strawberry-perl-*-portable/perl -maxdepth 1 -name bin`
+echo "PerlPath: $PerlPath"
+export PATH="$PerlPath:$PATH"
+
+echo "RootDir: $RootDir"
+echo "RootOutputDir: $RootOutputDir"
+echo "DistributionDir: $DistributionDir"
+
+pushd "$ScriptDir"
+
+if [[ "$BuildIncremental" != "true" ]]; then
+	rm -rf "$RootOutputDir/build"
+	rm -rf "$DistributionDir/"*
+fi
+
+mkdir -p "$RootOutputDir/build"
+pushd "$RootOutputDir/build"
+	cmd.exe //C `cygpath -m "$MalterlibRoot/External/llvm-project/llvm/utils/release/build_llvm_release.bat"` --version $LLVMVersion --$MalterlibArch --skip-checkout --local-python
+	SourceDistributionDir=`find "$RootOutputDir/build"/llvm_package*/build*/_CPack_Packages/*/NSIS/LLVM* -maxdepth 0 -type d`
+
+	"$MToolExecutable" DiffCopy `cygpath -m "$SourceDistributionDir/"`"*" "$DistributionDir"
+popd
