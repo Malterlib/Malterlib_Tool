71807a75c83884ef3903bb4e809d60fc7c3c8dd4
Erik Olofsson <erik@olofsson.info>
2026-06-26 23:53:34 +0200

    Update import cache

---
+++ ImportCache/External_CMake-Windows-x86-Debug/Source/cmVersionConfig.h
@@ -0,0 +1,10 @@
+/* Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+   file LICENSE.rst or https://cmake.org/licensing for details.  */
+#pragma once
+
+#define CMake_VERSION_MAJOR 4
+#define CMake_VERSION_MINOR 1
+#define CMake_VERSION_PATCH 2
+#define CMake_VERSION_SUFFIX "g59b0ccf"
+#define CMake_VERSION_IS_DIRTY 0
+#define CMake_VERSION "4.1.2-g59b0ccf"
